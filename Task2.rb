81349ea1b02ae2f0d29b10ab2c321e0e67adc7d2
MaksymBobryshev <kcmame@gmail.com>
2019-06-25 23:02:31 +0300

    working circle draw. Problem with radius 1. And need some refacroting

--- Task2.rb
+++ Task2.rb
@@ -6,27 +6,33 @@ def makeIndent2(r, i,indent = 25)
   (' ' * indent + ' ' * (i*2 - r*2))
 end
 def drawCircle(r = 5, indent = 25)
+  horisontalSpaces = r * 4 # r*2*2
   (1..r*2 + 1).each do |i|
-    if i == 1 || i == r*2
+    if i == 1 || i == r*2 + 1
       # puts('* * *'.rjust(indent, ' '))
       # makeIndent(r*2+1)
-      puts (makeIndent(r, i-1, indent) + '* * *')
+      if i < r
+        puts(makeIndent(r, i-1, indent) + '* * *')
+      else
+        puts(makeIndent2(r, i-1, indent) + '* * *')
+      end
     elsif i == r + 1
       # puts( ('**' + ' '*r*4 + '**').rjust(indent, ' '))
       # puts i, r
-      puts ( makeIndent(0, 0, indent) + ' **' + ' '*r*4 + '**')
+      puts( makeIndent(0, 0, indent) + ' **' + ' '*r*4 + '**')
     elsif i <= r
       # puts( ('*' + ' '*i*4 + '*').ljust(indent, ' ') )
 
-      puts (makeIndent(r, i-1, indent) + '*' + ' '*(i)*4 + '*')
+      puts(makeIndent(r, i-1, indent) + '*' + ' '*(i)*4 + '*')
     else
       # puts (makeIndent(r, (i-(i%r)), indent) + '*' + ' '*((i-i%r)+1)*4 + '*')
-      puts (makeIndent2(r, i, indent) + '*' + ' '*(((i*2 - r*2)*2)*4) + '*')
+      puts(makeIndent2(r, i-1, indent) + '*' + ' '*horisontalSpaces + '*')
+      horisontalSpaces -= 4
     end
   end
 end
 
-return drawCircle(5, 20)
+return drawCircle(4, 50)
 
 =begin
 
