e6215a96ad9388d1d42286ec60ef82f513b670ac
MaksymBobryshev <kcmame@gmail.com>
2019-06-25 23:21:07 +0300

    Some refactoring done

--- Task2.rb
+++ Task2.rb
@@ -1,38 +1,29 @@
 #Нарисовать в консоле круг, диаметр\радиус которого задается с консоли.
-def makeIndent(r, i,indent = 25)
-  (' ' * indent + ' ' *  (r*2 - (i)*2))
+def make_left_spaces(r, line_number, indent = 25)
+  if line_number < r
+    (' ' * indent + ' ' *  (r*2 - (line_number)*2))
+  else
+    (' ' * indent + ' ' * (line_number*2 - r*2))
+  end
 end
-def makeIndent2(r, i,indent = 25)
-  (' ' * indent + ' ' * (i*2 - r*2))
-end
-def drawCircle(r = 5, indent = 25)
-  horisontalSpaces = r * 4 # r*2*2
-  (1..r*2 + 1).each do |i|
-    if i == 1 || i == r*2 + 1
-      # puts('* * *'.rjust(indent, ' '))
-      # makeIndent(r*2+1)
-      if i < r
-        puts(makeIndent(r, i-1, indent) + '* * *')
-      else
-        puts(makeIndent2(r, i-1, indent) + '* * *')
-      end
-    elsif i == r + 1
-      # puts( ('**' + ' '*r*4 + '**').rjust(indent, ' '))
-      # puts i, r
-      puts( makeIndent(0, 0, indent) + ' **' + ' '*r*4 + '**')
-    elsif i <= r
-      # puts( ('*' + ' '*i*4 + '*').ljust(indent, ' ') )
 
-      puts(makeIndent(r, i-1, indent) + '*' + ' '*(i)*4 + '*')
-    else
-      # puts (makeIndent(r, (i-(i%r)), indent) + '*' + ' '*((i-i%r)+1)*4 + '*')
-      puts(makeIndent2(r, i-1, indent) + '*' + ' '*horisontalSpaces + '*')
-      horisontalSpaces -= 4
+def drawCircle(r = 5, indent = 25)
+  internal_spaces = r * 4 # r*2*2
+  (1..r * 2 + 1).each do |line_number|
+    if line_number == 1 || line_number == r*2 + 1  # top and bottom line of circle
+        puts(make_left_spaces(r, line_number-1, indent) + '* * *')
+    elsif line_number == r + 1  #middle line of circle
+      puts( make_left_spaces(0, 0, indent) + ' **' + ' '*r*4 + '**')
+    elsif line_number <= r    #top half lines of circle
+      puts(make_left_spaces(r, line_number-1, indent) + '*' + ' ' * (line_number) * 4 + '*')
+    else      # bottom half lines of circle
+      puts(make_left_spaces(r, line_number-1, indent) + '*' + ' ' * internal_spaces + '*')
+      internal_spaces -= 4
     end
   end
 end
 
-return drawCircle(4, 50)
+drawCircle(3, 50)
 
 =begin
 
