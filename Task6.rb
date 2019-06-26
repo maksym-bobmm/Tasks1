d215c0de679c9877b1033f40f49ec4a492daa55a
MaksymBobryshev <kcmame@gmail.com>
2019-06-26 12:58:00 +0300

    done 6

--- Task6.rb
+++ Task6.rb
@@ -10,9 +10,23 @@ get_all(array, Integer)
 => [1, 2, 3, 4, 111, 222, 333, 444, 100000]
 =end
 
-def get_all(array, type)
-
+def get_all(array, type, result_array = Array.new)
+  array.each do |item|
+    # puts item, item.class.to_s, type, "\n"
+    if item.class.to_s == 'Array' || item.class.to_s == 'Hash'
+      if item.count > 0
+        get_all(item, type, result_array)
+      end
+    elsif item.class.to_s == type
+      result_array << item
+    end
+  end
+  return result_array
 end
 
 array = [[1, 2, 3, 4, '1'], %w[2 5 10], [111, 222, 333, 444], %w[i love ruby], { key: 'value' }, [[['text', 100_000]]]]
 array.display
+puts "\n\n"
+get_all(array, 'Integer').display
+puts
+get_all(array, 'String').display
